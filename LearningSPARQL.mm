da8042b32c5c67aceb64fb40da9642ae4fac2ba4
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2024-10-03 12:03:18 +0800

    Update LearningSPARQL.mm

--- LearningSPARQL.mm
+++ LearningSPARQL.mm
@@ -1233,9 +1233,9 @@
 </node>
 </node>
 <node TEXT="03.12 Combining Values and Assigning Values to Variables" ID="ID_336702797" CREATED="1702255095657" MODIFIED="1702255107523"/>
-<node TEXT="03.13 Creating Tables of Values in Your Queries" ID="ID_300341315" CREATED="1702255108082" MODIFIED="1702255124643">
+<node TEXT="03.13 Creating Tables of Values in Your Queries" FOLDED="true" ID="ID_300341315" CREATED="1702255108082" MODIFIED="1702255124643">
 <node TEXT="VALUES in SQL" ID="ID_87169660" CREATED="1727872020453" MODIFIED="1727872029124">
-<node TEXT="The VALUES command specifies the valus of an INSERT INTO statement" ID="ID_1656414008" CREATED="1727872030611" MODIFIED="1727872052842">
+<node TEXT="The VALUES command specifies the values of an INSERT INTO statement" ID="ID_1656414008" CREATED="1727872030611" MODIFIED="1727928108986">
 <node TEXT="Example:&#xa;INSERT INTO Customers (CustomerName, ContactName, Address, City, PostalCode, Country)&#xa;VALUES (&apos;Cardinal&apos;, &apos;Tom B. Erichsen&apos;, &apos;Skagen 21&apos;, &apos;Stavanger&apos;, &apos;4006&apos;, &apos;Norway&apos;);" ID="ID_1922006202" CREATED="1727872052849" MODIFIED="1727872106587"/>
 </node>
 </node>
@@ -1243,7 +1243,20 @@
 <node TEXT="The VALUES ?var {var1...} clause generates a variable with one or more values. The values can be of any type, like numeric, strings, or even items." ID="ID_654511693" CREATED="1727872290844" MODIFIED="1727872476156"/>
 <node TEXT="SPARQL allows users to write queries against data that follow the RDF specification of the W3C." ID="ID_949263006" CREATED="1727872476817" MODIFIED="1727872498427"/>
 <node TEXT="The response to a SPARQL query contains an array of dictionaries, each corresponding to one node matching the conditions of the query." ID="ID_474124347" CREATED="1727872498615" MODIFIED="1727872537237"/>
-<node TEXT="UNDEF keyword acts as a wildcard, accepting any value that may come up." ID="ID_151830332" CREATED="1727922007732" MODIFIED="1727922032960"/>
+<node TEXT="UNDEF keyword acts as a wildcard, accepting any value that may come up." ID="ID_151830332" CREATED="1727922007732" MODIFIED="1727922032960">
+<node TEXT="If a variables has no value for a particular solution in the VALUES clause, the keyword UNDEF is used instead of and RDF term" ID="ID_1742798607" CREATED="1727922812361" MODIFIED="1727922849066" LINK="https://www.w3.org/TR/sparql11-query/#inline-data-examples"/>
+</node>
+</node>
+<node TEXT="How to ignore VALUES in SPARQL" ID="ID_1545343678" CREATED="1727922687312" MODIFIED="1727922877302" LINK="https://stackoverflow.com/questions/50135910/how-to-ignore-values-in-sparql">
+<node TEXT="FILTER (?ut IN ($ut))" ID="ID_1404060035" CREATED="1727922694739" MODIFIED="1727922712073">
+<node TEXT="passing $ut instead of a list of URIs" ID="ID_334746963" CREATED="1727922712076" MODIFIED="1727922730825"/>
+</node>
+<node TEXT="BIND ($ut as ?ut)" ID="ID_667537332" CREATED="1727922731705" MODIFIED="1727922740455">
+<node TEXT="passing $ut instead of a single URI" ID="ID_1576787343" CREATED="1727922740459" MODIFIED="1727922749753"/>
+</node>
+<node TEXT="VALUES (?ut) {(UNDEF)}" ID="ID_1602788472" CREATED="1727922750480" MODIFIED="1727922761551">
+<node TEXT="passing &quot;(UNDEF)&quot; instead of a space-separated list of (parentheses-enclosed) URIs" ID="ID_531307706" CREATED="1727922761555" MODIFIED="1727922792739"/>
+</node>
 </node>
 </node>
 <node TEXT="03.14 Sorting, Aggregating, Finding the Biggest and Smallest and..." FOLDED="true" ID="ID_391436192" CREATED="1702255169146" MODIFIED="1702255195420">
